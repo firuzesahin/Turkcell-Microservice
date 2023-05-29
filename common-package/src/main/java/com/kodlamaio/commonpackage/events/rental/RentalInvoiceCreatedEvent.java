d396ce09219353d2ec295e0c97c2c2034f2d4009
Firuze <firuzz_@outlook.com>
2023-05-29 12:15:34 +0300

    service-development-workshops

--- common-package/src/main/java/com/kodlamaio/commonpackage/events/rental/RentalInvoiceCreatedEvent.java
+++ common-package/src/main/java/com/kodlamaio/commonpackage/events/rental/RentalInvoiceCreatedEvent.java
@@ -1,26 +1,10 @@
 package com.kodlamaio.commonpackage.events.rental;
 
 import com.kodlamaio.commonpackage.events.Event;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.time.LocalDate;
+import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class RentalInvoiceCreatedEvent implements Event
 {
-    private String modelName;
-    private String brandName;
-    private String plate;
-    private int modelYear;
-    private double dailyPrice;
-    private double totalPrice;
-    private String cardHolder;
-    private int rentedForDays;
-    private LocalDate rentedAt;
+    private UUID carId;
 }
